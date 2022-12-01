d03fc2ff259ac4ffeb4a97755d0ab5634b0aa058
Andriy Sachok <andriy.sachok@gmail.com>
2022-12-01 20:14:14 +0200

    created connection between <LossesSelector /> and <DateRangePicker />

--- src/components/chartsHolder/LossesSelector.js
+++ src/components/chartsHolder/LossesSelector.js
@@ -6,74 +6,103 @@ import ErrorModal from "../UI/ErrorModal";
 import DateRangePickerDemo from "./inputChangers/DateRangePicker";
 
 const LossesSelector = (props) => {
-  const [month, setMonth] = useState("");
+  const [dateRange, setDateRange] = useState({ startDate: null, range: null });
   const [loss, setLoss] = useState("");
   const [error, setError] = useState(false);
 
-  let API = "";
-
-  switch (month) {
-    case "February":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=0&limit=3";
-      break;
-    case "March":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=3&limit=31";
-      break;
-    case "April":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=34&limit=30";
-      break;
-    case "May":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=64&limit=31";
-      break;
-    case "June":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=95&limit=30";
-      break;
-    case "July":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=125&limit=31";
-      break;
-    case "August":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=156&limit=31";
-      break;
-    case "September":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=187&limit=30";
-      break;
-    case "October":
-      API = "https://russianwarship.rip/api/v1/statistics?offset=217&limit=31";
-      break;
-    default:
-      break;
-  }
+  const millisecondsToDays = (milliseconds) => {
+    return milliseconds / 1000 / 24 / 60 / 60;
+  };
 
   useEffect(() => {
-    if (month !== "") {
-      fetchData();
-    }
-  }, [month, loss]);
+    calcCurrentAPI();
+  }, [dateRange.startDate]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(API);
+  const calcCurrentAPI = () => {
+    const firstDayMilliseconds = new Date("2022-02-24")
+      .toISOString()
+      .split("T")[0];
+    // const offsetDay = millisecondsToDays(
+    //   dateRange.startDate - firstDayMilliseconds
+    // );
 
-      const data = await response.json();
-
-      const date = data.data.records.map((element) => element.date);
-      const losses = {
-        title: loss,
-        amount: data.data.records.map((element) => element.stats[`${loss}`]),
-      };
-
-      props.sendData(date, losses);
-    } catch (error) {
-      setError(error.message);
-    }
+    console.log("firstDayMilliseconds: ", firstDayMilliseconds);
+    console.log("dateRange.startDate: ", dateRange.startDate);
+    // console.log("offsetDay: ", offsetDay);
   };
 
-  const setMonthState = (month) => {
-    setMonth(() => month);
-  };
+  // const minDateMilliseconds = new Date("2022-02-24").getTime();
+
+  // console.log(minDateMilliseconds);
+  // console.log(startDate);
+  // console.log(millisecondsToDays(startDate));
+  // let API = "";
+
+  // switch (month) {
+  //   case "February":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=0&limit=3";
+  //     break;
+  //   case "March":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=3&limit=31";
+  //     break;
+  //   case "April":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=34&limit=30";
+  //     break;
+  //   case "May":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=64&limit=31";
+  //     break;
+  //   case "June":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=95&limit=30";
+  //     break;
+  //   case "July":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=125&limit=31";
+  //     break;
+  //   case "August":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=156&limit=31";
+  //     break;
+  //   case "September":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=187&limit=30";
+  //     break;
+  //   case "October":
+  //     API = "https://russianwarship.rip/api/v1/statistics?offset=217&limit=31";
+  //     break;
+  //   default:
+  //     break;
+  // }
+
+  // useEffect(() => {
+  //   if (dateRange.startDate !== "") {
+  //     fetchData();
+  //   }
+  // }, [dateRange.startDate, loss]);
+
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch(API);
+
+  //     const data = await response.json();
+
+  //     const date = data.data.records.map((element) => element.date);
+  //     const losses = {
+  //       title: loss,
+  //       amount: data.data.records.map((element) => element.stats[`${loss}`]),
+  //     };
+
+  //     props.sendData(date, losses);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  // };
 
   const setLossState = (loss) => {
     setLoss(() => loss);
+  };
+
+  const getDateRange = ({ startDate, range }) => {
+    setDateRange(() => ({
+      startDate,
+      range,
+    }));
   };
 
   return (
@@ -87,8 +116,7 @@ const LossesSelector = (props) => {
           }}
         />
       )}
-      {/* <MonthChanger getMonth={setMonthState} /> */}
-      <DateRangePickerDemo />
+      <DateRangePickerDemo getDateRange={getDateRange} />
       <LossChanger getLoss={setLossState} />
     </Box>
   );
--- src/components/chartsHolder/inputChangers/DateRangePicker.js
+++ src/components/chartsHolder/inputChangers/DateRangePicker.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
@@ -8,24 +9,26 @@ import { DateRangePicker } from "@mui/x-date-pickers-pro";
 
 import { differenceInDays } from "date-fns";
 
-// const testDate = () => {
-//   let date1 = new Date("2022-11-24");
-//   let date2 = new Date(date1 - daysToMilliseconds(1));
-//   console.log("date1: ", date1);
-//   console.log("date2: ", date2.toString());
-// };
-
-// testDate();
-
-const DateRangePickerDemo = () => {
+const DateRangePickerDemo = (props) => {
   const [dates, setDates] = React.useState([null, null]);
 
   const daysToMilliseconds = (days) => {
     return days * 24 * 60 * 60 * 1000;
   };
 
+  useEffect(() => {
+    if (dates[0] !== null && dates[1] !== null) {
+      props.getDateRange({
+        startDate: dates[0]["$d"].getTime(),
+        range: getDateRangeLength(dates[1]["$d"], dates[0]["$d"]),
+      });
+    }
+  }, [props, dates[0]]);
+
+  // refactor this block of code
   const disableOutOfRange = (date) => {
     let currentDateToMilliseconds = date["$d"].getTime();
+
     if (dates[0] !== null) {
       let topBorderDateMilliseconds =
         dates[0]["$d"].getTime() + daysToMilliseconds(50);
@@ -35,6 +38,7 @@ const DateRangePickerDemo = () => {
       } else if (dates[1] !== null) {
         let botBorderDateMilliseconds =
           dates[1]["$d"].getTime() - daysToMilliseconds(50);
+
         if (currentDateToMilliseconds <= botBorderDateMilliseconds) {
           return true;
         } else return false;
@@ -45,14 +49,6 @@ const DateRangePickerDemo = () => {
   const getDateRangeLength = (end, start) => {
     return 1 + differenceInDays(end, start);
   };
-
-  const printDateRangeLength = (end, start) => {
-    console.log("Date range (in days): ", getDateRangeLength(end, start));
-  };
-
-  if (dates[0] !== null && dates[1] !== null) {
-    printDateRangeLength(dates[1]["$d"], dates[0]["$d"]);
-  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
