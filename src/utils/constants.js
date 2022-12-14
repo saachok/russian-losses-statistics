bbd66834bd25687f0dedceac54126405afe88476
Andriy Sachok <andriy.sachok@gmail.com>
2022-12-14 20:27:40 +0200

    added colors to constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,89 +7,148 @@ export const INITIAL_CHART_DATA = {
   losses: [{ label: "", data: [] }],
 };
 
+export const LOSSES_COLORS = {
+  brown: {
+    backgroundColor: "rgba(168, 46, 46, 0.85)",
+    borderColor: "rgba(168, 46, 46, 1)",
+  },
+  orange: {
+    backgroundColor: "rgba(211, 142, 52, 0.85)",
+    borderColor: "rgba(211, 142, 52, 1)",
+  },
+  yellow: {
+    backgroundColor: "rgba(208, 211, 52, 0.85)",
+    borderColor: "rgba(208, 211, 52, 1)",
+  },
+  darkYellow: {
+    backgroundColor: "rgba(211, 195, 52, 0.85)",
+    borderColor: "gba(211, 195, 52, 1)",
+  },
+  lightGreen: {
+    backgroundColor: "rgba(168, 211, 52, 0.85)",
+    borderColor: "rgba(168, 211, 52, 1)",
+  },
+  green: {
+    backgroundColor: "rgba(110, 211, 52, 0.85)",
+    borderColor: "rgba(110, 211, 52, 1)",
+  },
+  darkGreen: {
+    backgroundColor: "rgba(52, 211, 92, 0.85)",
+    borderColor: "rgba(52, 211, 92, 1)",
+  },
+  lightBlue: {
+    backgroundColor: "rgba(52, 211, 203, 0.85)",
+    borderColor: "rgba(52, 211, 203, 1)",
+  },
+  blue: {
+    backgroundColor: "rgba(52, 139, 211, 0.85)",
+    borderColor: "rgba(52, 139, 211, 1)",
+  },
+  darkBlue: {
+    backgroundColor: "rgba(52, 89, 211, 0.85)",
+    borderColor: "rgba(52, 89, 211, 1)",
+  },
+  darkPurple: {
+    backgroundColor: "rgba(76, 52, 211, 0.85)",
+    borderColor: "rgba(76, 52, 211, 1)",
+  },
+  purple: {
+    backgroundColor: "rgba(126, 52, 211, 0.85)",
+    borderColor: "rgba(126, 52, 211, 1)",
+  },
+  pink: {
+    backgroundColor: "rgba(206, 52, 211, 0.85)",
+    borderColor: "rgba(206, 52, 211, 1)",
+  },
+  coral: {
+    backgroundColor: "rgba(211, 52, 92, 0.85)",
+    borderColor: "rgba(211, 52, 92, 1)",
+  },
+};
+
 export const LOSSES_MAP = {
   personnel_units: {
     label: "Personnel units",
-    backgroundColor: "rgba(168, 46, 46, 0.6)",
-    borderColor: "rgba(168, 46, 46, 1)",
+    backgroundColor: LOSSES_COLORS.brown.backgroundColor,
+    borderColor: LOSSES_COLORS.brown.borderColor,
   },
 
   tanks: {
     label: "Tanks",
-    backgroundColor: "rgba(211, 142, 52, 0.6)",
-    borderColor: "rgba(211, 142, 52, 1)",
+    backgroundColor: LOSSES_COLORS.orange.backgroundColor,
+    borderColor: LOSSES_COLORS.orange.borderColor,
   },
 
   armoured_fighting_vehicles: {
     label: "Armoured flighting vehicles",
-    backgroundColor: "rgba(208, 211, 52, 0.6)",
-    borderColor: "rgba(208, 211, 52, 1)",
+    backgroundColor: LOSSES_COLORS.yellow.backgroundColor,
+    borderColor: LOSSES_COLORS.yellow.borderColor,
   },
 
   artillery_systems: {
     label: "Artillery systems",
-    backgroundColor: "rgba(211, 195, 52, 0.6)",
-    borderColor: "gba(211, 195, 52, 1)",
+    backgroundColor: LOSSES_COLORS.darkYellow.backgroundColor,
+    borderColor: LOSSES_COLORS.darkYellow.borderColor,
   },
 
   mlrs: {
     label: "MLRS",
-    backgroundColor: "rgba(168, 211, 52, 0.6)",
-    borderColor: "rgba(168, 211, 52, 1)",
+    backgroundColor: LOSSES_COLORS.lightGreen.backgroundColor,
+    borderColor: LOSSES_COLORS.lightGreen.borderColor,
   },
 
   aa_warfare_systems: {
     label: "AA warfare systems",
-    backgroundColor: "rgba(110, 211, 52, 0.6)",
-    borderColor: "rgba(110, 211, 52, 1)",
+    backgroundColor: LOSSES_COLORS.green.backgroundColor,
+    borderColor: LOSSES_COLORS.green.borderColor,
   },
 
   planes: {
     label: "Planes",
-    backgroundColor: "rgba(52, 211, 92, 0.6)",
-    orderdColor: "rgba(52, 211, 92, 1)",
+    backgroundColor: LOSSES_COLORS.darkGreen.backgroundColor,
+    borderColor: LOSSES_COLORS.darkGreen.borderColor,
   },
 
   helicopters: {
     label: "Helicopters",
-    backgroundColor: "rgba(52, 211, 203, 0.6)",
-    borderColor: "rgba(52, 211, 203, 1)",
+    backgroundColor: LOSSES_COLORS.lightBlue.backgroundColor,
+    borderColor: LOSSES_COLORS.lightBlue.borderColor,
   },
 
   vehicles_fuel_tanks: {
     label: "Vehicles and fuel tanks",
-    backgroundColor: "rgba(52, 139, 211, 0.6)",
-    borderColor: "rgba(52, 139, 211, 1)",
+    backgroundColor: LOSSES_COLORS.blue.backgroundColor,
+    borderColor: LOSSES_COLORS.blue.borderColor,
   },
 
   warships_cutters: {
     label: "Warships/cutters",
-    backgroundColor: "rgba(52, 89, 211, 0.6)",
-    orderdColor: "rgba(52, 89, 211, 1)",
+    backgroundColor: LOSSES_COLORS.darkBlue.backgroundColor,
+    borderColor: LOSSES_COLORS.darkBlue.borderColor,
   },
 
   cruise_missiles: {
     label: "Cruise missiles",
-    backgroundColor: "rgba(76, 52, 211, 0.6)",
-    orderdColor: "rgba(76, 52, 211, 1)",
+    backgroundColor: LOSSES_COLORS.darkPurple.backgroundColor,
+    borderColor: LOSSES_COLORS.darkPurple.borderColor,
   },
 
   uav_systems: {
     label: "UAV systems",
-    backgroundColor: "rgba(126, 52, 211, 0.6)",
-    borderColor: "rgba(126, 52, 211, 1)",
+    backgroundColor: LOSSES_COLORS.purple.backgroundColor,
+    borderColor: LOSSES_COLORS.purple.borderColor,
   },
 
   special_military_equip: {
     label: "Special military equipment",
-    backgroundColor: "rgba(206, 52, 211, 0.6)",
-    borderColor: "rgba(206, 52, 211, 1)",
+    backgroundColor: LOSSES_COLORS.pink.backgroundColor,
+    borderColor: LOSSES_COLORS.pink.borderColor,
   },
 
   atgm_srbm_systems: {
     label: "ATGM/SRBM systems",
-    backgroundColor: "rgba(211, 52, 92, 0.6)",
-    orderdColor: "rgba(211, 52, 92, 1)",
+    backgroundColor: LOSSES_COLORS.coral.backgroundColor,
+    borderColor: LOSSES_COLORS.coral.borderColor,
   },
 };
 
