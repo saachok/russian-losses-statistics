60a9de4fe04ba02646e2582753c13e34c88c385d
Andriy Sachok <andriy.sachok@gmail.com>
2022-12-25 11:42:42 +0200

    resolve problem

--- src/utils/themeChanging.js
+++ src/utils/themeChanging.js
@@ -1,7 +1,28 @@
-import {
-  DEFAULT_MODE,
-  DARK_MODE,
-  DARK_PALETTE,
-  LIGHT_MODE,
-  LIGHT_PALETTE,
-} from "./constants";
+import { DEFAULT_MODE, DARK_PALETTE, LIGHT_PALETTE } from "./constants";
+
+export const getThemeMode = (mode) => {
+  return mode === "dark" ? DARK_PALETTE : LIGHT_PALETTE;
+};
+
+export const getInitialTheme = () => {
+  let curMode = localStorage.getItem("mode");
+  if (!curMode) {
+    return DEFAULT_MODE;
+  } else {
+    return curMode;
+  }
+};
+
+export const switchToDark = () => {
+  localStorage.setItem("mode", "dark");
+  return "dark";
+};
+
+export const switchToLight = () => {
+  localStorage.setItem("mode", "light");
+  return "light";
+};
+
+export const switchTheme = (mode, handleChange) => {
+  handleChange(mode === "dark" ? switchToLight() : switchToDark());
+};
